c664204048717512dabee2be3c3bf3b543b066b9
kirillovme <54503407+kirillovme@users.noreply.github.com>
2020-07-21 15:51:01 +0300

    optimized money getting process

--- util/moneycurs.py
+++ util/moneycurs.py
@@ -1,13 +1,26 @@
+import json
+
 import requests
 
-global_request = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
-global_data = global_request.json()
-usd_price = global_data["Valute"]["USD"]["Value"]
-eur_price = global_data["Valute"]["EUR"]["Value"]
-cad_price = global_data["Valute"]["CAD"]["Value"]
+
+def get_course(currency):
+    global_request = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
+    global_data = json.loads(global_request.text)['Valute']
+    return global_data[currency]['Value']
+
+
+def convert(exchange_currency, value):
+    return float(value / float(get_course(exchange_currency)))
+
 
 currencies = {
-	"USA Dollar": "USD",
-	"Euro": "EUR",
-	"Canadian Dollar": "CAD"
+    "USA Dollar": "USD",
+    "Euro": "EUR",
+    "Canadian Dollar": "CAD"
+}
+
+exchange_currencies = {
+    "dollar": "USD",
+    "euro": "EUR",
+    "cdollar": "CAD"
 }
